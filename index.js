7ec142c799e23adbca9a45853a2602471f2c8b13
Christine Skorenko <cskorenko@hotmail.com>
2017-08-14 20:31:13 -0500

    js update2

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const emoji = require('node-emoji');
+const http = require('http');
 
-const emojiArray = ['coffee', 'pizza', 'heart', 'diamonds', 'spades',
-'clubs', 'sunrise', 'tulip', 'beer', 'custard'];
-
-emojiArray.forEach(function(item) {
-  console.log(emoji.get(item));
+const server = http.createServer((req, res) => {
+  res.write(emoji.random().emoji);
+  res.end();
 });
 
-function emojiLoop (array) {
-  for(let i=0; i < array.length; i++) {
-    console.log(emoji.get(array[i]));
-  }
-}
+server.listen(8001);
 
-function emojiRandom() {
-  for(let i=0; i<11; i++) {
-    console.log(emoji.random());
-  }
-}
 
-emojiLoop(emojiArray);
-emojiRandom();
+// const emojiArray = ['coffee', 'pizza', 'heart', 'diamonds', 'spades',
+// 'clubs', 'sunrise', 'tulip', 'beer', 'custard'];
+//
+// emojiArray.forEach(function(item) {
+//   console.log(emoji.get(item));
+// });
+//
+// function emojiLoop (array) {
+//   for(let i=0; i < array.length; i++) {
+//     console.log(emoji.get(array[i]));
+//   }
+// }
+//
+// function emojiRandom() {
+//   for(let i=0; i<11; i++) {
+//     console.log(emoji.random().emoji);
+//   }
+// }
+//
+// emojiLoop(emojiArray);
+// emojiRandom();
